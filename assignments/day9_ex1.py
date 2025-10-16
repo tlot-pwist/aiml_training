e7f62addc368d7e43fc6b4baf84f0c9e3bb63ad8
tlot-pwist <double.quote@gmail.com>
2025-10-16 12:15:10 +0800

    submit exercise the biggest of three numbers

---
+++ assignments/day9_ex1.py
@@ -0,0 +1,20 @@
+# Write a program using functions to find greatest of three numbers entered by user
+# Write the solution in Assignments folder with name
+# day_9_ex1.py
+# Push on GitHub
+# Share GitHub link in chat once done
+
+def biggest_of_three(a, b, c):
+    if a>b and a>c:
+        biggest=a
+    elif b>a and b>c:
+        biggest=b
+    else:
+        biggest=c
+    return biggest
+
+num1,num2,num3=map(int, input('enter 3 numbers with spcae between them: ').split())
+print(f'the biggest number is: {biggest_of_three(num1,num2,num3)}')
+
+
+    
